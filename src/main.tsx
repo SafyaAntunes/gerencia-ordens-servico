40923b5b3961e369b4f32af82eef5da77715bc32
Safya <safya.antunes@sistemafiepe.org.br>
2025-05-22 17:09:43 -0300

    q

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from './hooks/useAuth'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { Toaster } from '@/components/ui/sonner'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <TooltipProvider>
+      <App />
+      <Toaster position="top-right" />
+    </TooltipProvider>
+  </AuthProvider>
+);
